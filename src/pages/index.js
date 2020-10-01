1bd590b5338be76c73e06f4340224dd98a10cd01
Steve Lewis <slewis@ldms.eu>
2020-10-01 13:41:35 +0100

    Finished lesson 2 on styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <div>
+    <Header />
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and code React apps</h1>
+        <h1>Learn to<br/>design and code React apps</h1>
         <p>Complete courses about the best tools and designs.</p>
         <Link to='/page-2/'>Watch the Video</Link>
       </div>
