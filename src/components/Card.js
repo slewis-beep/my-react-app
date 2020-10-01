a31f19c14b068ca7814aa8747b772b63f64def7b
Steve Lewis <slewis@ldms.eu>
2020-10-01 15:19:51 +0100

    Finished Lesson 4 on SVG animations and started Less 5 Components

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Card = propers => (
+    <div className="Card">
+        <h3>React for Designers</h3>
+        <p>12 Sections</p>
+    </div>
+)
+
+export default Card
