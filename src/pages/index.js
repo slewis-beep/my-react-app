d2fe1cf879bdb252e261f1a8cbd257de1b632da5
Steve Lewis <slewis@ldms.eu>
2020-10-01 12:37:35 +0100

    Finished Lesson 2 of course. Basic styling for Hero content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,15 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Learn to design and code using React</h1>
-    <p>Hello boyo!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and designs.</p>
+        <Link to='/page-2/'>Watch the Video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-  </Layout>
+  </div>
 )
 
 export default IndexPage
